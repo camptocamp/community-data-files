1982a3bd1ad5c193b4b40ef54b6bc11ab9ff492b
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2020-07-28 18:23:01 +0300

    fixup! [13.0][ADD] l10n_eu_product_report: create

--- l10n_eu_adr_report/report/dangerous_delivery_report.py
+++ l10n_eu_adr_report/report/dangerous_delivery_report.py
@@ -18,33 +18,26 @@ class DangerousDeliveryADR(models.AbstractModel):
             "doc_model": "stock.picking",
             "docs": docs,
             "data": data.get("form", False),
-            # Amount of 7 lines is to satisfy requirements for first page
-            # as this amount is fiting A4 page
-            "first_page_lines": lines[:7],
-            "next_page_lines": lines[7:],
+            "page_lines": lines,
         }
         return docargs
 
     def _prepare_dangerous_lines(self, pickings):
-        # TODO report needs fixing as for now it cannot prepare any values for
-        # the report
-        pass
-        # vals = []
-        # pickings.ensure_one()
-        # for move_line in pickings.move_line_ids:
-        #     if move_line.product_id.dangerous_component_ids:
-        #         vals += self._get_dangerous_class_line_vals(move_line)
-        # return vals
+        vals = []
+        pickings.ensure_one()
+        for move_line in pickings.move_line_ids:
+            if move_line.product_id.is_dangerous:
+                vals += self._get_dangerous_class_line_vals(move_line)
+        return vals
 
     def _get_dangerous_class_line_vals(self, move):
-        vals = []
-        for component in move.product_id.dangerous_component_ids:
-            product = component.component_product_id
-            vals.append(
-                {
-                    "name": product.name,
-                    "class": product.get_full_class_name(),
-                    "gross_weight": move.move_id.weight,
-                }
-            )
-        return vals
+        product = move.product_id
+        return [
+            {
+                "name": product.name,
+                "class": product.product_tmpl_id.get_full_class_name(),
+                # quantity_done
+                "move_amount": move.move_id.product_uom_qty,
+                "product_weight": product.content_package,
+            }
+        ]
